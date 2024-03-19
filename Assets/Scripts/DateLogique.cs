ff155022c4e09aaf4b924ef0ce86feb922534d8b
Manon Goffinet <manon.goffinet@edu.devinci.fr>
2024-03-19 17:28:32 +0100

    DebutMenuFinalScene

---
+++ Assets/Scripts/DateLogique.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class DateLogique : MonoBehaviour
+{
+    public TMP_Text dateResult;
+    public void btnnum(char num)
+    {
+        if (dateResult.text.Contains('.'))
+        {
+            bool test = false;
+            string date = "1";
+            for (int i = 0; i < 3 && !test; i++)
+            {
+
+                if (dateResult.text[i] == '.' && !test)
+                {
+                    test = true;
+                    date += num;
+                }
+                else
+                {
+                    date += dateResult.text[i];
+                }
+            }
+            dateResult.text = date;
+
+        }
+        else
+        {
+
+        }
+
+    }
+    public void btn0()
+    {
+        btnnum('0');
+    }
+    public void btn1()
+    {
+        btnnum('1');
+    }
+    public void btn2()
+    {
+        btnnum('2');
+    }
+    public void btn3()
+    {
+        btnnum('3');
+    }
+    public void btn4()
+    {
+        btnnum('4');
+    }
+    public void btn5()
+    {
+        btnnum('5');
+    }
+    public void btn6()
+    {
+        btnnum('6');
+    }
+    public void btn7()
+    {
+        btnnum('7');
+    }
+    public void btn8()
+    {
+        btnnum('8');
+    }
+    public void btn9()
+    {
+        btnnum('9');
+    }
+
+    public void btnReturn()
+    {
+        bool test = false;
+        int index = 0;
+        string date = "";
+        for (int i = 0; i < 3 && !test; i++)
+        {
+
+            if (dateResult.text[i] == '.' && !test)
+            {
+                test = true;
+                index = i;
+            }
+        }
+        for (int i = 0; i <= index && !test; i++)
+        {
+            if (i == index - 1)
+            {
+                date += '.';
+            }
+            else
+            {
+                date += dateResult.text[i];
+            }
+        }
+        dateResult.text = date;
+    }
+
+    void Maj()
+    {
+        
+    }
+
+}
