ff65b4295dd48ab11a9c344504b7b57a8ce2d151
Manon Goffinet <manon.goffinet@edu.devinci.fr>
2024-03-26 12:28:35 +0100

    Revert "jsplus"
    This reverts commit 9b410a819645c6d1bb89d0e532a3d0057cb4719f.

---
+++ Assets/Scripts/FinMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinMenu : MonoBehaviour
+{
+    public Button okButton;
+    public TMP_Text Result;
+    public 
+    void Start()
+    {
+        Result.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
